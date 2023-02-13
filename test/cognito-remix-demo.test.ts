e59895e2ead2c06e98848021fca5f63b04a9be03
Mark O'Donnell <mark@dropdin.com>
2023-02-13 23:32:39 +0000

    Initial commit

---
+++ test/cognito-remix-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CognitoRemixDemo from '../lib/cognito-remix-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cognito-remix-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CognitoRemixDemo.CognitoRemixDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
